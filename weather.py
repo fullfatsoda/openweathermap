0e0d2729ac85987114c3da48d36eb06ae1e019d6
Christopher <65589469+fullfatsoda@users.noreply.github.com>
2020-06-28 16:16:50 +0100

    Formatted code to better conform with PEP8

--- weather.py
+++ weather.py
@@ -3,9 +3,9 @@
 
 import requests
 
-city_name   = '' # name of the city
-country     = '' # use ISO 3166 country code e.g. GB
-api_key     = '' # enter your api key here
+city_name = 'Leicester'  # name of the city
+country = 'GB'  # use ISO 3166 country code e.g. GB
+api_key = '8f0da511343891f24d89ac7302015193'  # enter your api key here
 
 # connect to the api
 get_weather = requests.get(
@@ -15,7 +15,7 @@ get_weather = requests.get(
     + country
     + '&appid='
     + api_key
-    )
+)
 # get the data in .json format
 get_weather_json = get_weather.json()
 
@@ -25,43 +25,48 @@ get_weather_json = get_weather.json()
 description = get_weather_json['weather'][0]['description']
 
 # temperature in celcius to 1 decimal place
-kelvin      = 273 # api temperatures are in kelvin (-273 to get celcius)
+kelvin = 273  # api temperatures are in kelvin (-273 to get celcius)
 temperature = round(((get_weather_json['main']['temp']) - kelvin), 1)
-feels_like  = round(((get_weather_json['main']['feels_like']) - kelvin), 1)
+feels_like = round(((get_weather_json['main']['feels_like']) - kelvin), 1)
 
 # pressure and humidity
-pressure    = get_weather_json['main']['pressure']
-humidity    = get_weather_json['main']['humidity']
+pressure = get_weather_json['main']['pressure']
+humidity = get_weather_json['main']['humidity']
 
 # wind speed - defauts to metres per second and degrees
 # converted to miles per hour and compass direction
-mph         = 2.2369 # mulitply by mph to convert from metres per second
-            # round wind speed to one decimal place
-wind_speed  = round(get_weather_json['wind']['speed'] * mph, 1)
+mph = 2.2369  # mulitply by mph to convert from metres per second
+# round wind speed to one decimal place
+wind_speed = round(get_weather_json['wind']['speed'] * mph, 1)
 wind_degree = get_weather_json['wind']['deg']
 
 # change from degree to compass direction
+
+
 def degree_to_direction(degree):
     # compass directions change for every 22.5 degrees
     # adding 0.5 is to ensure the value can't be equal to two directions
-    value       = int((degree / 22.5) + 0.5)
-    directions  = [
-                "N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE",
-                "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW",
-                ]
-    return directions[(value % 16)] # 16 directions
+    value = int((degree / 22.5) + 0.5)
+    directions = [
+        "N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE",
+        "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW",
+    ]
+    return directions[(value % 16)]  # 16 directions
+
 
 # rain fallen in the past hour (mm) and cloud coverage as a %
-try: # if there is any rain
+try:  # if there is any rain
     rain = get_weather_json['rain']['1h']
 except:
-    rain = 0 # there is no rain
+    rain = 0  # there is no rain
 
-clouds  = get_weather_json['clouds']['all']
+clouds = get_weather_json['clouds']['all']
 
 # display the information in a readable format
 print(f"Weather for {city_name}\nToday there will be {description}")
-print(f"Current temperature is {temperature} C but will feel like {feels_like} C")
+print(
+    f"Current temperature is {temperature} C but will feel like {feels_like} C")
 print(f"Atmospheric pressure {pressure} - Humidity {humidity}%")
-print(f"Wind speed {wind_speed}mph coming from {degree_to_direction(wind_degree)}")
+print(
+    f"Wind speed {wind_speed}mph coming from {degree_to_direction(wind_degree)}")
 print(f"{rain}mm of rain fallen in the last hour and {clouds}% cloud coverage")
